e831109b34d7ad04d82ccb7283d71e0b74b583df
Peter Mays <peter@mays.io>
2014-07-24 10:49:58 -0400

    Add homepage, css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple PHP App</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	
+	<body>
+		<h1>Homepage Placeholder</h1>
+		<p>&nbsp;</p>
+		<p><?php echo date("D M d"); ?></p>
+	</body>
+</html>
